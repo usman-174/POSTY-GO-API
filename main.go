424391ed663476dffefffb49279ff8cda1fd5a1d
Muhmmad Usman <hellmughal123@gmail.com>
2021-06-11 05:06:04 +0500

    uploading api

--- main.go
+++ main.go
@@ -4,20 +4,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/rs/cors"
 	"github.com/usman-174/app"
 )
 
 func main() {
 	server := app.Router()
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	clientUrl := os.Getenv("CLIENT_URL")
+	port := os.Getenv("PORT")
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedOrigins:   []string{clientUrl},
 		AllowCredentials: true,
 	})
 
 	handler := c.Handler(server)
-	log.Fatal(http.ListenAndServe(":4000", handler))
+	log.Fatal(http.ListenAndServe(port, handler))
 	fmt.Println("SERVER UP AND RUNNING")
 
 }
