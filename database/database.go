64fbb6bb7fd0f44b0b3b44e338559f275779ac26
Muhmmad Usman <hellmughal123@gmail.com>
2021-06-15 12:22:43 +0500

    fixing2

--- database/database.go
+++ database/database.go
@@ -15,7 +15,7 @@ import (
 func ConnectDataBase() *gorm.DB {
 	fmt.Println("STARTED DATABASE.go")
 	env := os.Getenv("ENV")
-	if env != "" || env == "development" {
+	if env == "development" {
 
 		err := godotenv.Load()
 		if err != nil {
