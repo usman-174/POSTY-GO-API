f09686b1bc36d63c8db708fc3cff79f397beedb2
Muhmmad Usman <hellmughal123@gmail.com>
2021-06-15 14:46:54 +0500

    fixing4

--- main.go
+++ main.go
@@ -23,10 +23,10 @@ func main() {
 		}
 		fmt.Println(".env file loaded")
 	}
-	clientUrl := os.Getenv("CLIENT_URL")
+	// clientUrl := os.Getenv("CLIENT_URL")
 	port := os.Getenv("PORT")
 	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{clientUrl},
+		AllowedOrigins:   []string{"*"},
 		AllowCredentials: true,
 	})
 	fmt.Println("port===", port)
